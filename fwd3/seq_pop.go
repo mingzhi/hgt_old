29e3fc5db2529c2949f8519194e8213e3793eaf4
Mingzhi Lin <mingzhi9@gmail.com>
2012-09-11 23:30:04 -0400

    change fwd3

--- fwd3/seq_pop.go
+++ fwd3/seq_pop.go
@@ -1,28 +1,37 @@
-// This package use gsl randist as random source.
-// This file implements SeqPop, which contains full sequence genomes.
-package fwd3
+// This file implements SeqPop, which is a population of full sequence genomes.
+// To use it:
+// 1. create a SeqPop object using NewSeqPop by providing population Size, genome Length, Mutation rate, Transfer rate, Fragment length, and a LockedSource.
+// 2. do the evoluation using Evolve
+package fwd1
 
 import (
-	"github.com/mingzhi/gsl-cgo/randist"
+	"github.com/mingzhi/gomath/random"
+	"math/rand"
+	"time"
 )
 
+// SeqPop: represents a population with full sequence genomes
 type SeqPop struct {
-	Size     int
-	Length   int
-	Fragment int
-	Mutation float64
-	Transfer float64
-	NumOfGen int
-
-	Genomes []Sequence
+	Size     int        // Population size
+	Length   int        // Genome length
+	Mutation float64    // Mutation rate
+	Transfer float64    // Transfer rate
+	Fragment int        // Transferred fragment
+	NumOfGen int        // Number of generation
+	Genomes  []Sequence // Store genomes
 
 	// private variables
-	states string
-	rng    *randist.RNG
+	src    rand.Source     // random sources (locked source)
+	rng    *rand.Rand      // random number generator with locked source
+	mPois  *random.Poisson // poisson random generator (mutator)
+	tPois  *random.Poisson // poisson random generator (transfer)
+	states string          // nucleotides
 }
 
+// Genome sequence
 type Sequence []byte
 
+// NewSeqPop: returns a SeqPop.
 func NewSeqPop(size, length int, mutation, transfer float64, fragment int) (pop *SeqPop) {
 	// verify size and length
 	if size <= 0 || length <= 0 {
@@ -38,20 +47,29 @@ func NewSeqPop(size, length int, mutation, transfer float64, fragment int) (pop 
 		Fragment: fragment,
 	}
 
-	// construct random source
-	pop.rng = randist.NewRNG(randist.MT19937)
+	// create random sources
+	seed := time.Now().UnixNano()
+	pop.src = random.NewLockedSource(rand.NewSource(seed))
+	pop.rng = rand.New(pop.src)
+	// mutation poisson distribution: mean = mutation rate * genome length
+	pop.mPois = random.NewPoisson(pop.Mutation*float64(pop.Length), pop.src)
+	// transfer poisson distribution: mean = transfer rate * genome length
+	pop.tPois = random.NewPoisson(pop.Transfer*float64(pop.Length), pop.src)
 
-	// construct genomes
+	// nucleotides
 	pop.states = "ATGC"
+	// create a random reference sequence
 	refseq := make(Sequence, pop.Length)
 	for i := 0; i < pop.Length; i++ {
-		s := pop.states[randist.UniformRandomInt(pop.rng, len(pop.states))]
+		s := pop.states[pop.rng.Intn(len(pop.states))]
 		refseq[i] = s
 	}
+	// create a population size of genomes by coping the reference sequence
 	pop.Genomes = make([]Sequence, pop.Size)
 	for i := 0; i < pop.Size; i++ {
-		pop.Genomes[i] = make(Sequence, pop.Length)
-		copy(pop.Genomes[i], refseq)
+		genome := make(Sequence, pop.Length)
+		copy(genome, refseq)
+		pop.Genomes[i] = genome
 	}
 
 	return
@@ -68,6 +86,7 @@ func (pop *SeqPop) Evolve() {
 	pop.NumOfGen++
 }
 
+// generation: wright-fisher generate a fixed number of population.
 func (pop *SeqPop) generation() {
 	// new generation
 	newGenomes := make([]Sequence, pop.Size)
@@ -75,13 +94,13 @@ func (pop *SeqPop) generation() {
 	// wright-fisher sampling
 	used := make([]bool, pop.Size) // indicates whether the pointer of a old genome has been used
 	for i := 0; i < pop.Size; i++ {
-		o := randist.UniformRandomInt(pop.rng, pop.Size) // sample with replacement
+		o := pop.rng.Intn(pop.Size) // sample with replacement
 		if used[o] {
 			newGenomes[i] = make(Sequence, pop.Length)
 			copy(newGenomes[i], pop.Genomes[o])
 		} else {
 			newGenomes[i] = pop.Genomes[o]
-			used[i] = true
+			used[o] = true
 		}
 	}
 
@@ -92,6 +111,12 @@ func (pop *SeqPop) generation() {
 // mutation: K2P model.
 // Each genome is independent with each other so that concurrency is implemented.
 func (pop *SeqPop) mutation() {
+	// in case that the mutation rate is changed
+	mean := pop.Mutation * float64(pop.Length)
+	if pop.mPois.Mean != mean {
+		pop.mPois.Mean = mean
+	}
+
 	// for each genome, do mutation
 	for i := 0; i < pop.Size; i++ {
 		pop.mutateGenome(i)
@@ -102,13 +127,13 @@ func (pop *SeqPop) mutation() {
 func (pop *SeqPop) mutateGenome(idx int) {
 	genome := pop.Genomes[idx]
 	// how many mutations in this genome (following a poisson distribution)
-	count := randist.PoissonRandomInt(pop.rng, pop.Mutation*float64(pop.Length))
+	count := pop.mPois.Int()
 	// for each mutation, randomly choose a position and replace it with another nucleotide.
 	for i := 0; i < count; i++ {
-		p := randist.UniformRandomInt(pop.rng, pop.Length)
-		s := pop.states[randist.UniformRandomInt(pop.rng, len(pop.states))]
+		p := pop.rng.Intn(pop.Length)
+		s := pop.states[pop.rng.Intn(len(pop.states))]
 		for s == genome[p] { // loop until a random nucleotide that is different to the original one
-			s = pop.states[randist.UniformRandomInt(pop.rng, len(pop.states))]
+			s = pop.states[pop.rng.Intn(len(pop.states))]
 		}
 		genome[p] = s
 	}
@@ -117,6 +142,12 @@ func (pop *SeqPop) mutateGenome(idx int) {
 // transfer
 // Warning: transaction (read and write) is not atomic and not safe.
 func (pop *SeqPop) transfer() {
+	// in case that the transfer rate is changed
+	mean := pop.Transfer * float64(pop.Length)
+	if pop.tPois.Mean != mean {
+		pop.tPois.Mean = mean
+	}
+
 	// for each genome, do transfer
 	for i := 0; i < pop.Size; i++ {
 		pop.transferGenome(i)
@@ -128,18 +159,18 @@ func (pop *SeqPop) transferGenome(idx int) {
 	receiver := pop.Genomes[idx]
 
 	// how many times of homologous recombination happen.
-	count := randist.PoissonRandomInt(pop.rng, pop.Transfer*float64(pop.Length))
+	count := pop.tPois.Int()
 	// for each transfer, a donor genome is randomly chose, then a fragment is randomly determined.
 	for c := 0; c < count; c++ {
 		// a different donor genome is randomly selected.
-		donor_id := randist.UniformRandomInt(pop.rng, pop.Size)
+		donor_id := pop.rng.Intn(pop.Size)
 		for donor_id == idx {
-			donor_id = randist.UniformRandomInt(pop.rng, pop.Size)
+			donor_id = pop.rng.Intn(pop.Size)
 		}
 		donor := pop.Genomes[donor_id]
 
 		// a fragment is randomly determined
-		fragL := randist.UniformRandomInt(pop.rng, pop.Length)
+		fragL := pop.rng.Intn(pop.Length)
 		fragR := fragL + pop.Fragment
 		if fragR < pop.Length {
 			for i := fragL; i < fragR; i++ {
