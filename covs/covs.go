6c126d3db21ba83750fe2d2f431e6e7cf7ba84a7
Mingzhi Lin <mingzhi9@gmail.com>
2012-09-15 12:29:00 -0400

    Cov and covs_test

--- covs/covs.go
+++ covs/covs.go
@@ -2,6 +2,7 @@ package covs
 
 import (
 	"github.com/mingzhi/gomath/stat/desc"
+	"sort"
 )
 
 type CMatrix struct {
@@ -26,5 +27,90 @@ func (cm *CMatrix) D() (m float64, v float64) {
 
 	m = mean.GetResult()
 	v = variance.GetResult()
+	return
+}
+
+// Calculate the covs.
+func (cm *CMatrix) Cov(maxL int) (scovs, rcovs, xyPL, xsysPL, smXYPL []float64) {
+	// calculate xs and xy
+	xs := make([]int, cm.Length) // total counts of each column.
+	xy := make([]int, maxL)      // total counts of cocurrence.
+	// sort each row
+	for i := 0; i < cm.Size; i++ {
+		sort.Ints(cm.Matrix[i])
+	}
+	// loop
+	for i := 0; i < cm.Size; i++ {
+		row := cm.Matrix[i]
+		for xi, xv := range row {
+			// add xs
+			xs[xv]++
+			// add xy
+			for yi := 0; yi < len(row); {
+				yv := row[yi]
+				if yv < xv { // deal with circle genome
+					if yv+cm.Length-xv < maxL {
+						xy[yv+cm.Length-xv]++
+						yi++
+					} else {
+						yi = xi
+					}
+				} else {
+					if yv-xv < maxL {
+						xy[yv-xv]++
+						yi++
+					} else {
+						break
+					}
+				}
+			}
+		}
+	}
+
+	// calculate xsP (frequency of xs)
+	xsP := make([]float64, len(xs))
+	for i := 0; i < len(xs); i++ {
+		xsP[i] = float64(xs[i]) / float64(cm.Size)
+	}
+
+	// calculate xyP (frequence of xy)
+	xyP := make([]float64, len(xy))
+	for i := 0; i < len(xy); i++ {
+		xyP[i] = float64(xy[i]) / float64(cm.Size*cm.Length)
+	}
+
+	xsysP := make([]float64, maxL) // <X.Y>
+	smXsP := make([]float64, maxL) // sum(X)
+	smYsP := make([]float64, maxL) // sum(Y)
+	for l := 0; l < maxL; l++ {
+		for x := 0; x < cm.Length; x++ {
+			if x+l >= cm.Length {
+				xsysP[l] += xsP[x] * xsP[l-(cm.Length-x)]
+				smXsP[l] += xsP[x]
+				smYsP[l] += xsP[l-(cm.Length-x)]
+			} else {
+				xsysP[l] += xsP[x] * xsP[x+l]
+				smXsP[l] += xsP[x]
+				smYsP[l] += xsP[x+l]
+			}
+		}
+		xsysP[l] /= float64(cm.Length)
+		smXsP[l] /= float64(cm.Length)
+		smYsP[l] /= float64(cm.Length)
+	}
+
+	scovs = make([]float64, maxL)
+	rcovs = make([]float64, maxL)
+	xyPL = make([]float64, maxL)
+	xsysPL = make([]float64, maxL)
+	smXYPL = make([]float64, maxL)
+	for l := 0; l < maxL; l++ {
+		scovs[l] = xyP[l] - xsysP[l]
+		rcovs[l] = xsysP[l] - smXsP[l]*smYsP[l]
+		xyPL[l] = xyP[l]
+		xsysPL[l] = xsysP[l]
+		smXYPL[l] = smXsP[l] * smYsP[l]
+	}
+
 	return
 }
---
+++ covs/covs_test.go
@@ -0,0 +1,83 @@
+package covs
+
+import (
+	"github.com/mingzhi/gomath/stat/desc"
+	"math"
+	"math/rand"
+	"testing"
+)
+
+func TestCov(t *testing.T) {
+	a := 0.9
+	b := 0.3
+	length := 1000
+	size := 10000
+	c := simuMarkovMatrix(a, b, size, length)
+	maxL := 100
+	scovs, rcovs, _, _, _ := c.Cov(maxL)
+	sterr := 0.0
+	for i := 0; i < maxL; i++ {
+		sc := scovs[i]
+		f := (1.0 - a) * (1.0 - b) / ((2.0 - a - b) * (2.0 - a - b)) * math.Pow((a+b-1.0), float64(i))
+		sterr += (sc - f) * (sc - f)
+	}
+	if sterr > 1e-6 {
+		t.Errorf("The standard error is too big: %g\n", sterr)
+	}
+
+	rmean := desc.NewMean()
+	for i := 0; i < maxL; i++ {
+		rmean.Increment(rcovs[i])
+	}
+	prec := 1e-6
+	if math.Abs(rmean.GetResult()) > prec {
+		t.Errorf("Expect average of rcovs as 0.0, but got %f", rmean.GetResult())
+	}
+}
+
+func BenchmarkCov(bench *testing.B) {
+	bench.StopTimer()
+	a := 0.9
+	b := 0.3
+	length := 1000
+	size := 10000
+	c := simuMarkovMatrix(a, b, size, length)
+	maxL := 100
+	bench.StartTimer()
+	for i := 0; i < bench.N; i++ {
+		c.Cov(maxL)
+	}
+}
+
+func simuMarkovMatrix(a, b float64, size, length int) (c *CMatrix) {
+	distmatrix := make([][]int, size)
+	for i := 0; i < size; i++ {
+		distmatrix[i] = simuMarkovProcess(a, b, length)
+	}
+
+	c = NewCMatrix(size, length, distmatrix)
+	return
+}
+
+func simuMarkovProcess(a, b float64, length int) (v []int) {
+	prev := 0
+	v = []int{}
+	for i := 0; i < length; i++ {
+		if prev == 0 {
+			if rand.Float64() < a {
+				prev = 0
+			} else {
+				v = append(v, i)
+				prev = 1
+			}
+		} else {
+			if rand.Float64() < b {
+				v = append(v, i)
+				prev = 1
+			} else {
+				prev = 0
+			}
+		}
+	}
+	return
+}
