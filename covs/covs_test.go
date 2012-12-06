d20075c581e78235b5580581c7f35a7bebb2b831
Mingzhi Lin <mingzhi9@gmail.com>
2012-12-06 00:54:15 -0500

    change covs_test

--- covs/covs_test.go
+++ covs/covs_test.go
@@ -1,124 +1,57 @@
 package covs
 
 import (
-	"fmt"
-	"github.com/mingzhi/gomath/stat/desc"
-	"math"
 	"math/rand"
-	"runtime"
 	"testing"
 )
 
-func TestCov(t *testing.T) {
-	a := 0.9
-	b := 0.3
-	length := 1000
-	size := 10000
-	c := simuMarkovMatrix(a, b, size, length)
-	maxL := 100
-	scovs, rcovs, _, _, _ := c.Cov(maxL)
-	sterr := 0.0
-	for i := 0; i < maxL; i++ {
-		sc := scovs[i]
-		f := (1.0 - a) * (1.0 - b) / ((2.0 - a - b) * (2.0 - a - b)) * math.Pow((a+b-1.0), float64(i))
-		sterr += (sc - f) * (sc - f)
-	}
-	if sterr > 1e-6 {
-		t.Errorf("The standard error is too big: %g\n", sterr)
-	}
-
-	rmean := desc.NewMean()
-	for i := 0; i < maxL; i++ {
-		rmean.Increment(rcovs[i])
-	}
-	prec := 1e-6
-	if math.Abs(rmean.GetResult()) > prec {
-		t.Errorf("Expect average of rcovs as 0.0, but got %g", rmean.GetResult())
-	}
-}
-
-func TestCalculateKS(t *testing.T) {
-	fmt.Println("--Population parameters:")
-	var (
-		n         int     // population size
-		u         float64 // mutation rate
-		r         float64 // transfer rate
-		a         int     // number of states
-		l         int     // fragment length
-		ks        float64
-		expected  float64
-		tolerance float64
-	)
-	n = 1000
-	u = 1e-4
-	r = 1e-4
-	a = 4
-	l = 100
-	tolerance = 1e-5
-	fmt.Printf("---Population size: %d\n", n)
-	fmt.Printf("---Mutation rate: %f\n", u)
-	fmt.Printf("---Transfer rate: %f\n", r)
-	fmt.Printf("---Character number: %d\n", a)
-	fmt.Printf("---Transferred fragment: %d\n", l)
-	ks = CalculateKS(n, u, r, l, a)
-	expected = 0.15667
-	if math.Abs(ks-expected) > tolerance {
-		t.Errorf("ks = %g, but expected %g", ks, expected)
-	}
-
-	// no transfer
-	r = 0.0
-	ks = CalculateKS(n, u, r, l, a)
-	expected = 0.157911
-	if math.Abs(ks-expected) > tolerance {
-		t.Errorf("ks = %g, but expected %g", ks, expected)
-	}
-}
-
-func BenchmarkCov(bench *testing.B) {
-	bench.StopTimer()
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	a := 0.9
-	b := 0.3
-	length := 1000
-	size := 10000
-	c := simuMarkovMatrix(a, b, size, length)
-	maxL := 100
-	bench.StartTimer()
-	for i := 0; i < bench.N; i++ {
-		c.Cov(maxL)
-	}
-}
-
-func simuMarkovMatrix(a, b float64, size, length int) (c *CMatrix) {
-	distmatrix := make([][]int, size)
-	for i := 0; i < size; i++ {
-		distmatrix[i] = simuMarkovProcess(a, b, length)
-	}
-
-	c = NewCMatrix(size, length, distmatrix)
-	return
-}
-
-func simuMarkovProcess(a, b float64, length int) (v []int) {
-	prev := 0
-	v = []int{}
-	for i := 0; i < length; i++ {
-		if prev == 0 {
-			if rand.Float64() < a {
-				prev = 0
-			} else {
-				v = append(v, i)
-				prev = 1
-			}
-		} else {
-			if rand.Float64() < b {
-				v = append(v, i)
-				prev = 1
-			} else {
-				prev = 0
+func TestCircleCov(t *testing.T) {
+	// create testing distance sequences
+	size := 10
+	leng := 1000
+	seqs := make([][]int, size)
+	dmatrix := make([][]int, size)
+	for i := 0; i < len(seqs); i++ {
+		seqs[i] = make([]int, leng)
+		dmatrix[i] = []int{}
+		for j := 0; j < len(seqs[i]); j++ {
+			d := rand.Intn(2)
+			seqs[i][j] = d
+			if d == 1 {
+				dmatrix[i] = append(dmatrix[i], j)
 			}
 		}
 	}
-	return
+
+	maxl := 100
+	n_xy := make([]int, maxl)
+	for l := 0; l < maxl; l++ {
+		for i := 0; i < len(seqs); i++ {
+			for j := 0; j < len(seqs[i]); j++ {
+				if j+l < len(seqs[i]) {
+					if seqs[i][j] == 1 && seqs[i][j+l] == 1 {
+						n_xy[l]++
+					}
+				} else {
+					if seqs[i][j] == 1 && seqs[i][j+l-len(seqs[i])] == 1 {
+						n_xy[l]++
+					}
+				}
+
+			}
+		}
+	}
+
+	p_xy := make([]float64, maxl)
+	for i := 0; i < maxl; i++ {
+		p_xy[i] = float64(n_xy[i]) / float64(size*leng)
+	}
+	cmatrix := NewCMatrix(size, leng, dmatrix)
+	_, _, xy, _, _ := cmatrix.CovCircle(maxl)
+	for i := 0; i < maxl; i++ {
+		if p_xy[i] != xy[i] {
+			t.Errorf("%d: p_xy = %g, xy = %g", i, p_xy[i], xy[i])
+		}
+	}
+
 }
